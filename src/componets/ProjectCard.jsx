93eff558190d6c4ba3785bb252e9aa7bc57c6e57
nicolas <juliantorres136@gmail.com>
2022-01-30 11:07:36 -0500

    add the projects cards

--- src/componets/ProjectCard.jsx
+++ src/componets/ProjectCard.jsx
@@ -1,140 +1,73 @@
-import React from 'react';
+import React from "react";
 import {
     Box,
-    IconButton,
-    useBreakpointValue,
-    Stack,
-    Heading,
+    Flex,
+    AspectRatio,
+    Image,
     Text,
-    Container,
-} from '@chakra-ui/react';
-// Here we have used react-icons package for the icons
-import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
-// And react-slick as our Carousel Lib
-import Slider from 'react-slick';
+    Link,
+    Button,
+    Stack, useColorModeValue, Center, Heading, Avatar
+} from "@chakra-ui/react";
 
-// Settings for the slider
-const settings = {
-    dots: true,
-    arrows: false,
-    fade: true,
-    infinite: true,
-    autoplay: true,
-    speed: 500,
-    autoplaySpeed: 5000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-};
+//import imgs
+import photoProjet1 from '../img/project1.png'
 
-export default function ProjectCard() {
-    // As we have used custom buttons, we need a reference variable to
-    // change the state
-    const [slider, setSlider] = React.useState<Slider | null>(null);
+function ProjetCard(props) {
+    const { product, summary, longLine } = props;
 
-    // These are the breakpoints which changes the position of the
-    // buttons as the screen size changes
-    const top = useBreakpointValue({ base: '90%', md: '50%' });
-    const side = useBreakpointValue({ base: '30%', md: '40px' });
 
-    // This list contains all the data for carousels
-    // This can be static or loaded from a server
-    const cards = [
-        {
-            title: 'Design Projects 1',
-            text:
-                "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
-            image:
-                'https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-        },
-        {
-            title: 'Design Projects 2',
-            text:
-                "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
-            image:
-                'https://images.unsplash.com/photo-1438183972690-6d4658e3290e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80',
-        },
-        {
-            title: 'Design Projects 3',
-            text:
-                "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
-            image:
-                'https://images.unsplash.com/photo-1507237998874-b4d52d1dd655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-        },
-    ];
 
     return (
-        <Box
-            position={'relative'}
-            height={'600px'}
-            width={'full'}
-            overflow={'hidden'}>
-            {/* CSS files for react-slick */}
-            <link
-                rel="stylesheet"
-                type="text/css"
-                charSet="UTF-8"
-                href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-            />
-            <link
-                rel="stylesheet"
-                type="text/css"
-                href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-            />
-            {/* Left Icon */}
-            <IconButton
-                aria-label="left-arrow"
-                variant="ghost"
-                position="absolute"
-                left={side}
-                top={top}
-                transform={'translate(0%, -50%)'}
-                zIndex={2}
-                onClick={() => slider?.slickPrev()}>
-                <BiLeftArrowAlt size="40px" />
-            </IconButton>
-            {/* Right Icon */}
-            <IconButton
-                aria-label="right-arrow"
-                variant="ghost"
-                position="absolute"
-                right={side}
-                top={top}
-                transform={'translate(0%, -50%)'}
-                zIndex={2}
-                onClick={() => slider?.slickNext()}>
-                <BiRightArrowAlt size="40px" />
-            </IconButton>
-            {/* Slider */}
-            <Slider {...settings} ref={(slider) => setSlider(slider)}>
-                {cards.map((card, index) => (
-                    <Box
-                        key={index}
-                        height={'6xl'}
-                        position="relative"
-                        backgroundPosition="center"
-                        backgroundRepeat="no-repeat"
-                        backgroundSize="cover"
-                        backgroundImage={`url(${card.image})`}>
-                        {/* This is the block you need to change, to customize the caption */}
-                        <Container size="container.lg" height="600px" position="relative">
-                            <Stack
-                                spacing={6}
-                                w={'full'}
-                                maxW={'lg'}
-                                position="absolute"
-                                top="50%"
-                                transform="translate(0, -50%)">
-                                <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
-                                    {card.title}
-                                </Heading>
-                                <Text fontSize={{ base: 'md', lg: 'lg' }} color="GrayText">
-                                    {card.text}
-                                </Text>
-                            </Stack>
-                        </Container>
-                    </Box>
-                ))}
-            </Slider>
-        </Box>
+        <Center py={6}>
+            <Box
+                maxW={'445px'}
+                w={'full'}
+                bg={useColorModeValue('white', 'gray.900')}
+                boxShadow={'2xl'}
+                rounded={'md'}
+                p={6}
+                overflow={'hidden'}>
+                <Box
+                    h={'210px'}
+                    bg={'gray.100'}
+                    mt={-6}
+                    mx={-6}
+                    mb={6}
+                    pos={'relative'}>
+                    <Image
+                        src={
+                            'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                        }
+                        layout={'fill'}
+                    />
+                </Box>
+                <Stack>
+                    <Text
+                        color={'#D95323'}
+                        textTransform={'uppercase'}
+                        fontWeight={800}
+                        fontSize={'sm'}
+                        letterSpacing={1.1}>
+                        Blog
+                    </Text>
+                    <Heading
+                        color={useColorModeValue('gray.700', 'white')}
+                        fontSize={'2xl'}
+                        fontFamily={'body'}>
+                        Boost your conversion rate
+                    </Heading>
+                    <Text color={'gray.500'}>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+                        et ea rebum.
+                    </Text>
+                </Stack>
+
+            </Box>
+        </Center>
     );
 }
+
+export default ProjetCard;
--- src/componets/ProjectGrid.jsx
+++ src/componets/ProjectGrid.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Box, Container, SimpleGrid, useColorMode} from "@chakra-ui/react";
-import ProjectCard from './ProjectCard';
+import ProjetCard from "./ProjectCard.jsx";
+import ProjectCard from "./ProjectCard.jsx";
 
 
 class HelloMessage extends React.Component {
     render() {
         return (
             <box>
-                <Container>
+                <Container  maxW='container.lg'>
                     <SimpleGrid columns={3} spacing={10}>
-                        <Box border='1px' roundedBottom={1} height='350px'></Box>
-                        <Box border='1px' roundedBottom={1} height='350px'></Box>
-                        <Box border='1px' roundedBottom={1} height='350px'></Box>
+                        <Box  ><ProjectCard/></Box>
+                        <Box  ><ProjectCard/></Box>
+                        <Box  ><ProjectCard/></Box>
 
                     </SimpleGrid>
                 </Container>
